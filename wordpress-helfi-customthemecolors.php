f450805ae5a571d5e9040e3a0525db2968e653d0
artcloudAntti <50194560+artcloudAntti@users.noreply.github.com>
2023-11-27 11:57:04 +0200

    Update wordpress-helfi-customthemecolors.php

--- wordpress-helfi-customthemecolors.php
+++ wordpress-helfi-customthemecolors.php
@@ -4,7 +4,7 @@
  * Description:     Select your own primary, secondary and accent color combinations for Helsinki Theme
  * Text Domain:     wordpress-helfi-customthemecolors
  * Domain Path:     /languages
- * Version:         0.1
+ * Version:         1.0.1
  *
  * @package Wordpress_Helfi_Customthemecolors
  */
@@ -14,7 +14,7 @@ namespace CityOfHelsinki\WordPress\CustomThemeColors;
 /**
  * Constants
 */
-define( __NAMESPACE__ . '\\PLUGIN_VERSION', '1.0.0' );
+define( __NAMESPACE__ . '\\PLUGIN_VERSION', '1.0.1' );
 define( __NAMESPACE__ . '\\PLUGIN_PATH', plugin_dir_path( __FILE__ ) );
 define( __NAMESPACE__ . '\\PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 define( __NAMESPACE__ . '\\PLUGIN_BASENAME', plugin_basename( __FILE__ ) );
